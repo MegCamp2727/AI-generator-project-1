09c360bde7a915954f553695ffcc2f48c55e7598
MegCamp2727 <MegCamp2727@gmail.com>
2025-03-13 11:31:04 -0400

    Changed background to image and linked GitHub account

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ event.preventDefault();
 
 let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "d90d35t42b8f499421c0b6fca6d4fo03";
-let prompt = `User instructions are: Generate a cocktail recipe about ${instructionsInput.value}`;
+let prompt = `User instructions are: Generate a cocktail recipe about ${instructionsInput.value} keep the resipe short and sweet and to the point.`;
 let context = 
 "You are a bartender who knows every cocktail recipe.";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
